4f36a73d339d69db82c2a9eaa64291681e471966
View490 <m6422040110@g.siit.tu.ac.th>
2023-07-19 07:10:02 +0700

    This is speech to text classification model built from tensorflow model training with pre-recorded wav files 'left right up down go stop' the model can be more accuracy if training with larger dataset or tuning it better.
    Then the model was saved and used with turtle library to command the turtle game play.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import tensorflow as tf
 from tensorflow.keras import models
 
 from recording_helper import record_audio, terminate
@@ -8,11 +8,15 @@ from tf_helper import preprocess_audiobuffer
 # !! Modify this in the correct order
 commands = ['left', 'down', 'stop', 'up', 'right', 'no', 'go', 'yes']
 
-loaded_model = models.load_model("saved_model")
+# loaded_model = models.load_model("saved_model")
+loaded_model = tf.saved_model.load("saved_model")
+
 
 def predict_mic():
     audio = record_audio()
     spec = preprocess_audiobuffer(audio)
+    # Add this line to reshape
+    spec = tf.convert_to_tensor(spec, dtype=tf.float32)
     prediction = loaded_model(spec)
     label_pred = np.argmax(prediction, axis=1)
     command = commands[label_pred[0]]
